035f596cf1ba1614746a3baa79d4beaf36ff0345
BHollinberger <brandon.hollinberger@co.platte.mo.us>
2016-04-25 11:31:54 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -74,7 +74,7 @@ map.on('locationfound', function(e) {
         properties: {
             "title": 'Here I Am',
             "marker-color": '#ff8888',
-            "marker-symbol": 'smiley face'
+            "marker-symbol": 'diamond'
         }
     });
 
