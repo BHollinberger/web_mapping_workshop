76175512af64f23d3d98a30bb35520e1d5c710d3
BHollinberger <brandon.hollinberger@co.platte.mo.us>
2016-04-25 09:05:06 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -2,7 +2,7 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'bhollinberger.pp97ef9b';
+var mapId = 'bhollinberger.pp9bof02';
 
 // And this is my access token, use yours.
 var accessToken = 'pk.eyJ1IjoiYmhvbGxpbmJlcmdlciIsImEiOiJjaW5nMGdkY2cxOXFqdjBrcTduemM2cXF4In0.s5J_YCA6oYunBD0TII7wPg';
